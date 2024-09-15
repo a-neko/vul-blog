9d0a87780b7d2137c1e7165ebcf37f333573b829
any <bluefin27pops@docomo.ne.jp>
2024-09-15 19:22:52 +0900

    add login page

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const mysql = require("mysql2/promise");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter=require('./routes/login');
 
 var app = express();
 
-
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  connectionLimit: 3, // 接続を張り続けるコネクション数を指定
+  namedPlaceholders: true, // 設定必須
+});
+module.exports=pool;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +34,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login',loginRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
