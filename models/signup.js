dae5562649300a5277aa8af84a9c62cf64bafffc
any <bluefin27pops@docomo.ne.jp>
2024-09-19 05:48:27 +0900

    インサート機能の実装

---
+++ models/signup.js
@@ -0,0 +1,15 @@
+const pool=require('../models/accessDB');
+
+function signup(req,res,next){
+  const insert_sql = `insert into user (name,password) values ('${req.body.name}','${req.body.password}')`;
+  const select_sql=`select * from user where name='${req.body.name}'`;
+  pool.query(select_sql).then((data)=>{
+    if(data[0][0]!=null){
+
+    }else{
+      console.log('1');
+      pool.query(insert_sql).then(()=>console.log('insert userdata'));
+    }
+  });
+}
+module.exports=signup;
---
+++ routes/signup.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var router = express.Router();
+const signup=require('../models/signup')
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('signup');
+});
+
+router.post('/', function(req, res, next) {
+  signup(req,res,next);
+  res.redirect('/blogs');
+});
+
+module.exports = router;
