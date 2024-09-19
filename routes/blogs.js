2f5c1fac2cc3838874708e7a3a047b95445a26f2
any <bluefin27pops@docomo.ne.jp>
2024-09-20 03:16:40 +0900

    全投稿画面を作成

--- routes/blogs.js
+++ routes/blogs.js
@@ -3,11 +3,13 @@ const express=require('express');
 const router=express.Router();
 const auth=require('../models/login');
 const is_login=require('../models/is_login')
+const getAllBlogs=require('../models/getAllblogs')
 
 router.get('/',is_login,function(req, res, next) {
-  // console.log(req);
 
-  res.render('blogs', { title: 'Express' });
+  getAllBlogs(req,res,next);
+
+
 });
 
 module.exports = router;
