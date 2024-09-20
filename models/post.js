862a778efec27ea5b27e2c4f11dce2ddc2ddaf7e
any <bluefin27pops@docomo.ne.jp>
2024-09-21 01:08:16 +0900

    getpostの実装

--- models/post.js
+++ models/post.js
@@ -1,7 +1,7 @@
 const pool = require('./accessDB');
 
 function postBlog(req, res, next) {
-  const userId = req.user.id;
+  const userId = req.user[0].id;
   const user = req.body;
   const sql = `insert into blog (userId,title,content,eyecatch,createdAt) values (${userId},'${user.title}','${user.content}',${user.eyecatch},cast(now() as datetime))`
   pool.query(sql).then(() => console.log('created new blog!'))
