8de964b939f98c2472772003e844a1337a4c02be
any <bluefin27pops@docomo.ne.jp>
2024-09-20 17:57:34 +0900

    viewsのfile構造を変化

--- models/getAllblogs.js
+++ models/getAllblogs.js
@@ -3,8 +3,8 @@ const pool=require('./accessDB');
 function getAllBlogs(req,res,next){
   const sql=`select blog.eyecatch,blog.title,user.name from user,blog where blog.userId=user.id;`
   pool.query(sql).then((data)=> data[0]).then((blogs)=>{
-    console.log(blogs);
-    res.render('blogs',{blogs: blogs})});
+
+    res.render('blogs/blogs',{blogs: blogs})});
 
 }
 module.exports=getAllBlogs;
