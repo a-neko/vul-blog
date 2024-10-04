a43fdf495f138569cf40c8e6a3c1dbacf62e8dd2
any <bluefin27pops@docomo.ne.jp>
2024-10-04 09:39:43 +0900

    xssを可能にするためにhttponlyをfalseに

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: false}));
+app.use(session({secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: false,cookie: {httpOnly: false}}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use('/', indexRouter);
